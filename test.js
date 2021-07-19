63781d53ac49ee99e9d9425e88bc52c96fad575d
Reese Schumacher <reesesch1@gmail.com>
2021-07-19 13:25:09 -0600

    added new test

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('can add another test and it still works!!!', () => {
+  expect(getGreeting('Reese')).toBe("Hi Reese");
+});
